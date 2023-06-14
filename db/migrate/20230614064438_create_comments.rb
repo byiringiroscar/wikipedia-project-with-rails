e57233cc5a7d3ab9a0325e7ab0810179c21e2531
byiringiroscar <byiringoroscar@gmail.com>
2023-06-14 09:23:38 +0200

    add indexes

--- db/migrate/20230614064438_create_comments.rb
+++ db/migrate/20230614064438_create_comments.rb
@@ -1,11 +1,11 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.string :author_id
-      t.string :post_id
-      t.text :Text
+      t.integer :author_id
+      t.integer :post_id
+      t.text :text
       t.datetime :updated_at
-      t.datetime :CreatedAt
+      t.datetime :created_at
 
       t.timestamps
     end
