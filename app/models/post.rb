1301232134d167cfa5c4874954afc43dbee08ca6
byiringiroscar <byiringoroscar@gmail.com>
2023-06-14 15:45:51 +0200

    Resolve review issues

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,14 @@ class Post < ApplicationRecord
   belongs_to :user, foreign_key: :author_id
   has_many :likes, foreign_key: :post_id
   has_many :comments, foreign_key: :post_id
-  def update_posts_counter
-    user.update(posts_counter: user.posts.count)
-  end
-
+  after_save :update_posts_counter
   def recent_comments(limit = 5)
     comments.order(created_at: :desc).limit(limit)
+  end
+
+  private
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
   end
 end
