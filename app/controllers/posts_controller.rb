9846506c87348dc570abb5604ac9662bdd768c9b
byiringiroscar <byiringoroscar@gmail.com>
2023-06-21 19:47:52 +0200

    Add data for post for users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find_by(id: params[:user_id])
+    @posts = @user&.posts
+    unless @user
+      flash.now[:error] = "User not found"
+    end
+  end
 
   def show; end
 end
