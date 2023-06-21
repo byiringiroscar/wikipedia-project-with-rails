8986834200967d2a26366d8160b2dd13a042fb87
byiringiroscar <byiringoroscar@gmail.com>
2023-06-22 00:41:03 +0200

    Add rounting and fetch in database

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,18 @@ class PostsController < ApplicationController
   def index
     @user = User.find_by(id: params[:user_id])
     @posts = @user&.posts
-    unless @user
-      flash.now[:error] = "User not found"
-    end
+    return if @user
+
+    flash.now[:error] = 'User not found'
   end
 
-  def show; end
+  def show
+    @user = User.find_by(id: params[:user_id])
+    if @user
+      @post = @user.posts.find_by(id: params[:id])
+      flash.now[:error] = 'Post not found' unless @post
+    else
+      flash.now[:error] = 'User not found'
+    end
+  end
 end
